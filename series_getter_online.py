3cddfd7d16e3e210a9973405a55f87d8a61f9c6b
Raj dubey <com.mailuser@gmail.com>
2020-02-18 14:13:46 +0530

    fix input

--- series_getter_online.py
+++ series_getter_online.py
@@ -5,13 +5,13 @@ import time
 from bs4 import BeautifulSoup as bs
 import getsize 
 url =' https://en.wikipedia.org/wiki/Mobile_telephone_numbering_in_India'
-print("Please Enter Values as Asked")
-print("Enter the Circle ...")
-state_to_extract=input()#if set to None all state is considered
-state_to_extract=state_to_extract.upper()
-telecom_to_extracted=None#if set to none all operator from particular city is extracted
-should_null_state_included=False #True or False only
-stateName="india" if state_to_extract==None else state_to_extract
+print("Please Enter Values as CircleName operatorName  [t/f](should_null_state_included)")
+ins=input().split()
+
+state_to_extract=ins[0].upper() if len(ins)>=1 else None#if set to None all state is considered
+telecom_to_extracted=ins[1].upper() if len(ins)>=2 else None #if set to none all operator from particular city is extracted
+should_null_state_included=True if len(ins)>=3 and ins[2].lower()=="t" else False #True or False only
+stateName="Whole india" if state_to_extract==None else state_to_extract
 opName="all_operator" if telecom_to_extracted==None else telecom_to_extracted
 nullcheck="WithNull" if should_null_state_included else "WithoutNull"
 print(f"currently accessing {stateName} for {opName} {nullcheck}")
