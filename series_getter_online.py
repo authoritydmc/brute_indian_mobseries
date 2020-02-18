b9d5fc78d92e243a7d1f9606b72ebbd35e397850
Raj dubey <com.mailuser@gmail.com>
2020-02-18 14:33:47 +0530

    fix 0 length file creation

--- series_getter_online.py
+++ series_getter_online.py
@@ -5,16 +5,27 @@ import time
 from bs4 import BeautifulSoup as bs
 import getsize 
 url =' https://en.wikipedia.org/wiki/Mobile_telephone_numbering_in_India'
-print("Please Enter Values as CircleName operatorName  [t/f](should_null_state_included)")
+
+print("Please Enter Values as\n:[CircleName] [operatorName]  [t/f](should_null_value included)")
+print("Enter n at the not needed place \n(only applicable for CircleName and operatorName)")
+print("ENTER---->",end="")
 ins=input().split()
 
-state_to_extract=ins[0].upper() if len(ins)>=1 else None#if set to None all state is considered
-telecom_to_extracted=ins[1].upper() if len(ins)>=2 else None #if set to none all operator from particular city is extracted
+state_to_extract=ins[0].upper() if len(ins)>=1 and ins[0].lower()!="n" else  None#if set to None all state is considered
+telecom_to_extracted=ins[1].upper() if len(ins)>=2 and ins[1].lower()!="n" else None #if set to none all operator from particular city is extracted
 should_null_state_included=True if len(ins)>=3 and ins[2].lower()=="t" else False #True or False only
 stateName="Whole india" if state_to_extract==None else state_to_extract
 opName="all_operator" if telecom_to_extracted==None else telecom_to_extracted
 nullcheck="WithNull" if should_null_state_included else "WithoutNull"
 print(f"currently accessing {stateName} for {opName} {nullcheck}")
+print("Enter yes or y to continue")
+opch=input()
+opch=opch.lower()
+print("You selected ",opch)
+if opch!="y" and opch!="yes":
+	print("TADA BYE BYE ")
+	time.sleep(3)
+	exit()
 print("Connecting...")
 response = requests.get(url)
 print(response)
@@ -85,6 +96,11 @@ for k in one_a_tag:
 filename=f"../out/{stateName}_{opName}-{nullcheck}.txt"
 lst.sort()
 cnt=0
+if len(lst)==0:
+	print("NO Valid Series Found\nExiting ")
+	time.sleep(3)
+	exit()
+
 with open(filename,"w") as f:
 	for z in lst:
 		if len(z)==4:
